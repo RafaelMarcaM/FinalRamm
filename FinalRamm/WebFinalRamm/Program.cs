3cad70b3d8dbe002ae598f9125bf4f6d81dfe43c
RafaleMarcaManrique <raphaelmarca@gmail.com>
2023-06-16 14:58:44 -0400

    Creación de MVC de productos y conexion a la base de datos

--- FinalRamm/WebFinalRamm/Program.cs
+++ FinalRamm/WebFinalRamm/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WebFinalRamm.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<FinalRammContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
