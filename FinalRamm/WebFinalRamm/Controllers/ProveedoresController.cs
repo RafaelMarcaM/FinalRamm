e28e9427baf59af7bf7d5ca76cc676fb08d71469
Rafael Marca <raphaelmarca@gmail.com>
2023-06-17 17:50:36 -0400

    CRUD Proveedores terminado

---
+++ FinalRamm/WebFinalRamm/Controllers/ProveedoresController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebFinalRamm.Models;
+
+namespace WebFinalRamm.Controllers
+{
+    [Authorize]
+    public class ProveedoresController : Controller
+    {
+        private readonly FinalRammContext _context;
+
+        public ProveedoresController(FinalRammContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Proveedors
+        public async Task<IActionResult> Index()
+        {
+              return _context.Proveedors != null ? 
+                          View(await _context.Proveedors.Where(x => x.RegistroActivo.Value).ToListAsync()) :
+                          Problem("Entity set 'FinalRammContext.Proveedors'  is null.");
+        }
+
+        // GET: Proveedors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Proveedors == null)
+            {
+                return NotFound();
+            }
+
+            var proveedor = await _context.Proveedors
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (proveedor == null)
+            {
+                return NotFound();
+            }
+
+            return View(proveedor);
+        }
+
+        // GET: Proveedors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Proveedors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nit,RazonSocial,Direccion,Telefono,Ciudad,Banco,CuentaBancaria,UsuarioRegistro,RegistroActivo,FechaRegistro")] Proveedor proveedor)
+        {
+            if (ModelState.IsValid)
+            {
+                proveedor.UsuarioRegistro = User.Identity?.Name;
+                proveedor.FechaRegistro = DateTime.Now;
+                proveedor.RegistroActivo = true;
+                _context.Add(proveedor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(proveedor);
+        }
+
+        // GET: Proveedors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Proveedors == null)
+            {
+                return NotFound();
+            }
+
+            var proveedor = await _context.Proveedors.FindAsync(id);
+            if (proveedor == null)
+            {
+                return NotFound();
+            }
+            return View(proveedor);
+        }
+
+        // POST: Proveedors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nit,RazonSocial,Direccion,Telefono,Ciudad,Banco,CuentaBancaria,UsuarioRegistro,RegistroActivo,FechaRegistro")] Proveedor proveedor)
+        {
+            if (id != proveedor.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    proveedor.UsuarioRegistro = User.Identity?.Name;
+                    proveedor.FechaRegistro = DateTime.Now;
+                    proveedor.RegistroActivo = true;
+                    _context.Update(proveedor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProveedorExists(proveedor.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(proveedor);
+        }
+
+        // GET: Proveedors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Proveedors == null)
+            {
+                return NotFound();
+            }
+
+            var proveedor = await _context.Proveedors
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (proveedor == null)
+            {
+                return NotFound();
+            }
+
+            return View(proveedor);
+        }
+
+        // POST: Proveedors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Proveedors == null)
+            {
+                return Problem("Entity set 'FinalRammContext.Proveedors'  is null.");
+            }
+            var proveedor = await _context.Proveedors.FindAsync(id);
+            if (proveedor != null)
+            {
+                //_context.Proveedors.Remove(proveedor);
+                proveedor.RegistroActivo = false;
+                _context.Update(proveedor);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProveedorExists(int id)
+        {
+          return (_context.Proveedors?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
